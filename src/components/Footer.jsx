25dda26b37bcf05b8656595dbb241a1fbe19f972
Shaik Farook <skfarook0203@gmail.com>
2026-01-04 21:01:57 +0530

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer style={{ backgroundColor: 'var(--primary)', color: 'var(--secondary)', padding: '10px', textAlign: 'center' }}>
+      <p>&copy; 2023 FLUSTAR. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
