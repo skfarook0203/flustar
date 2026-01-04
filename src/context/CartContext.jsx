6a5d24e48fc61b13a393782e8db6b4de2f1afbb9
Shaik Farook <skfarook0203@gmail.com>
2026-01-04 20:58:36 +0530

    Create CartContext.jsx

---
+++ src/context/CartContext.jsx
@@ -0,0 +1,34 @@
+import React, { createContext, useState, useEffect } from 'react';
+
+export const CartContext = createContext();
+
+export const CartProvider = ({ children }) => {
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    const savedCart = localStorage.getItem('cart');
+    if (savedCart) setCart(JSON.parse(savedCart));
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('cart', JSON.stringify(cart));
+  }, [cart]);
+
+  const addToCart = (product) => {
+    setCart([...cart, product]);
+  };
+
+  const removeFromCart = (id) => {
+    setCart(cart.filter(item => item.id !== id));
+  };
+
+  const updateQuantity = (id, quantity) => {
+    setCart(cart.map(item => item.id === id ? { ...item, quantity } : item));
+  };
+
+  return (
+    <CartContext.Provider value={{ cart, addToCart, removeFromCart, updateQuantity }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
