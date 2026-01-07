e22094db8fb86a025a7a9c998b2607616c7bcc64
Shaik Farook <skfarook0203@gmail.com>
2026-01-07 23:09:58 +0530

    Update ProductCart.jsx

--- src/components/ProductCart.jsx
+++ src/components/ProductCart.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const ProductCard = ({ product }) => {
   return (
     <div style={{ border: '1px solid #ccc', padding: '10px', borderRadius: '5px' }}>
-      <img src={`https://your-render-backend-url.com/uploads/${product.images[0]}`} alt={product.name} style={{ width: '100%', height: '200px', objectFit: 'cover' }} />
+      <img src={`https://flustar-backend.onrender.com/uploads/${product.images[0]}`} alt={product.name} style={{ width: '100%', height: '200px', objectFit: 'cover' }} />
       <h3>{product.name}</h3>
       <p>₹{product.price}</p>
       <Link to={`/product/${product._id}`}>View Details</Link>
